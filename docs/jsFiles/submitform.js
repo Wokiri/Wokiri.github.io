34b2a3e3891a7fe9d443a436a83e95d035aed5a3
Joe Oketch <wokirijoe@gmail.com>
2021-01-05 15:16:45 +0300

    Added page head

--- docs/jsFiles/submitform.js
+++ docs/jsFiles/submitform.js
@@ -1,14 +1,13 @@
 const clientName = document.getElementById('client_name').value
 const clientEmail = document.getElementById('client_email').value
 const clientMessage = document.getElementById('client_message').value
-const submitForm = document.getElementById('submitForm')
+const inquiryForm = document.getElementById('inquiryForm')
 
-submitForm.addEventListener('click', sendEmail)
 
 function sendEmail(){
     Email.send({
         SecureToken : "328ca5c9-e164-47f4-b584-a40539c09334",
-        To : 'wokirijoe@gmail.com',
+        To : ['wokirijoe@gmail.com', clientEmail],
         From : clientEmail,
         Subject : `You have a Project Inquiry from ${clientName}`,
         Body : clientMessage
@@ -16,3 +15,5 @@ function sendEmail(){
     message => alert(message)
     );
 }
+
+submitForm.addEventListener('submit', sendEmail)
